0662eeda86a7fbaabd32471c800e1d5e74178c2e
sean <wenxiong.su@qunar.com>
2017-07-04 17:19:20 +0800

    add group controller and models

--- server/controllers/group.js
+++ server/controllers/group.js
@@ -5,12 +5,12 @@ module.exports = {
     async add(ctx) {
         let params = ctx.request.body;
         if(!params.group_name){
-            return ctx.body = yapi.commons.resReturn(null, 400, 'Group不能为空');
+            return ctx.body = yapi.commons.resReturn(null, 400, '组名不能为空');
         }
         var checkRepeat = await groupModel.checkRepeat(params.group_name);
-        // if(checkRepeat > 0){
-        //     return ctx.body =  yapi.commons.resReturn(null, 401, 'group已存在');
-        // }
+        if(checkRepeat > 0){
+            return ctx.body =  yapi.commons.resReturn(null, 401, '组名已存在');
+        }
         let data = {
             group_name: params.group_name,
             group_desc: params.group_desc,
@@ -29,15 +29,37 @@ module.exports = {
     },
 
     async list(ctx) {
-        console.log(yapi)
-        ctx.body = 1;
+        try{
+            let result = await groupModel.list();
+            ctx.body = yapi.commons.resReturn(result)
+        }catch(err){
+             ctx.body = yapi.commons.resReturn(null, 402, e.message)
+        }
     },
 
     async del(ctx){   
-
+        try{
+            let id = ctx.request.body.id;
+            let result = await groupModel.del(id);
+            ctx.body = yapi.commons.resReturn(result)
+        }catch(err){
+             ctx.body = yapi.commons.resReturn(null, 402, e.message)
+        }
     },
 
     async up(ctx){
-
+        try{
+            let id = ctx.request.body.id;
+            let data = {};
+            ctx.request.body.group_name && (data.group_name = ctx.request.body.group_name)
+            ctx.request.body.group_desc && (data.group_desc = ctx.request.body.group_desc)
+            if(Object.keys(data).length ===0){
+                ctx.body = yapi.commons.resReturn(null, 404, '分组名和分组描述都为空');
+            }
+            let result = await groupModel.up(id, data);
+            ctx.body = yapi.commons.resReturn(result)
+        }catch(err){
+             ctx.body = yapi.commons.resReturn(null, 402, e.message)
+        }
     }
 }
--- server/models/group.js
+++ server/models/group.js
@@ -1,33 +1,41 @@
 import yapi from '../yapi.js'
-const groupSchema = ({
+const groupSchema = {
     uid: String,
     group_name: String,
     group_desc: String,
     add_time: Number,
     up_time: Number
-})
-
-
-var groupModel = yapi.db('group', groupSchema);    
-
-
-function save(data){
-    let m = new groupModel(data);
-    return m.save();
 }
 
-function checkRepeat(name){
-    return groupModel.count({
-        group_name: name
-    })    
-}
 
-function list(){
-    return groupModel.list().exec()
-}
+var groupModel = yapi.db('group', groupSchema);
 
-module.exports = {   
-    save: save,
-    checkRepeat: checkRepeat,
-    list: list
+
+module.exports = {
+    save: (data) => {
+        let m = new groupModel(data);
+        return m.save();
+    },
+    checkRepeat: (name) => {
+        return groupModel.count({
+            group_name: name
+        })
+    },
+    list: () => {
+        return groupModel.find().select("group_name _id group_desc add_time up_time").exec()
+    },
+    del: (id) => {
+        return groupModel.deleteOne({
+            _id: id
+        })
+    },
+    up: (id, data) => {
+        return groupModel.update({
+            _id: id,
+        }, {
+            group_name: data.group_name,
+            group_desc: data.group_desc,
+            up_time: yapi.commons.time()
+        })
+    }
 }   
