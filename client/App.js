e14f22c94b62afcf7c0c3967273a9c73c111e1bf
waliang.wang <waliang.wang@qunar.com>
2017-07-05 16:35:31 +0800

    添加热启动

--- client/App.js
+++ client/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Routes from './routes'
 import './styles/cs.scss'
 // import './styles/cs.less'
+console.log(4444)
 
 class App extends Component {
   render() {
