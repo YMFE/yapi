dbcf9ed02ca630a40e1c45a518483e77929aff49
waliang.wang <waliang.wang@qunar.com>
2017-07-06 17:01:48 +0800

    添加common.scss

--- client/containers/Home/Home.js
+++ client/containers/Home/Home.js
@@ -1,12 +1,39 @@
-import React from 'react'
-import { Button } from 'antd';
+import '../../styles/Home/home.scss'
+import React, { Component } from 'react'
+import { Button, Input, Icon, Checkbox } from 'antd'
 import Header from '../../components/Header/Header.js'
 
-export default function Home() {
-  return (
-    <acticle>
-      <Header />
-      <Button type="primary">Primary</Button>
-    </acticle>
-  )
+class Home extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render () {
+    return (
+      <acticle>
+        <Header />
+
+        <h3>YAPI</h3>
+
+        <div className="home-box">
+          <Input
+            size="large"
+            placeholder="Username"
+            prefix={<Icon type="user" style={{ fontSize: 14 }} />} />
+
+          <Input
+            size="large"
+            type="password"
+            placeholder="Password"
+            prefix={<Icon type="lock" style={{ fontSize: 14 }} />} />
+
+          <Button type="primary">登录</Button>
+        </div>
+
+        <Checkbox>记住密码</Checkbox>
+      </acticle>
+    )
+  }
 }
+
+export default Home
