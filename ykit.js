9a9809bca9633e33b2ec952b501efd530772f9ba
waliang.wang <waliang.wang@qunar.com>
2017-07-06 14:25:52 +0800

    优化package.json

--- ykit.js
+++ ykit.js
@@ -1,25 +1,25 @@
 var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-// var extractCSS = new ExtractTextPlugin('stylesheets/[name].css');
 
 module.exports = {
-    // plugins: ['react', 'es6', 'antd'],
-    plugins: ['react', 'qunar', 'es6'],
-    devtool:  'cheap-source-map',
-    config: {
-        exports: [
-            './index.js'
-        ],
-        modifyWebpackConfig: function(baseConfig) {
-            baseConfig.context = path.resolve(__dirname, "client");
-            baseConfig.watch = true;
-            return baseConfig;
-        }
-    },
-    // server: {
-    //     hot: true, // true/false，默认 false，效果相当于 ykit server --hot
-    //     overlay: true // true/false，默认 false，开启后可在当前打开的页面提示打包错误
-    // },
-    hooks: {},
-    commands: []
+  plugins: ['qunar', 'antd'],
+  devtool:  'cheap-source-map',
+  config: {
+    exports: [
+      './index.js'
+    ],
+    modifyWebpackConfig: function(baseConfig) {
+      baseConfig.context = path.resolve(__dirname, "client");
+      baseConfig.watch = true;
+      return baseConfig;
+    }
+  },
+  server: {
+    // true/false，默认 false，效果相当于 ykit server --hot
+    hot: true,
+    // true/false，默认 false，开启后可在当前打开的页面提示打包错误
+    overlay: true 
+  },
+  hooks: {},
+  commands: []
 };
