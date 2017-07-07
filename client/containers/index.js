f5d98a46b90b865280546e9d6cec46bedd0e20c2
waliang.wang <waliang.wang@qunar.com>
2017-07-07 17:26:31 +0800

    添加首页导航

--- client/containers/index.js
+++ client/containers/index.js
@@ -1,7 +1,9 @@
 import Home from './Home/Home.js'
+import Login from './Login/Login.js'
 import ProjectGroups from './ProjectGroups/ProjectGroups.js'
 
 export {
   Home,
+  Login,
   ProjectGroups
 }
