4214299dfa41c31a3a5f29a585d4c1b9ca99d183
zwjamnsss <zwjamnsss@gmail.com>
2017-07-10 20:28:03 +0800

    router

--- client/routes.js
+++ client/routes.js
@@ -2,10 +2,11 @@ import React from 'react'
 import { Route, HashRouter, Redirect } from 'react-router-dom'
 import { Home, Login, ProjectGroups } from './containers/index'
 
-export default store => {
+export default () => {
   return (
     <HashRouter>
       <div className="router-main">
+        <Redirect from="/" to="/ProjectGroups" />
         <Route path="/" component={ Home } />
         <Route path="/Login" component={ Login } />
         <Route path="/ProjectGroups" component={ ProjectGroups } />
