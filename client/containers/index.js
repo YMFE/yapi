80cadec422985372a98137fc9507f7917e469d99
zwjamnsss <zwjamnsss@gmail.com>
2017-07-11 10:22:06 +0800

    polyfill

--- client/containers/index.js
+++ client/containers/index.js
@@ -1,5 +1,5 @@
 import Home from './Home/Home.js'
-import Login from './Login/Login_container.js'
+import Login from './Login/Login.js'
 import ProjectGroups from './ProjectGroups/ProjectGroups.js'
 
 export {
