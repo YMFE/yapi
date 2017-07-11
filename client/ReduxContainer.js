2eeac22a48e10358ffa89c7ebd066390e6eb638e
waliang.wang <waliang.wang@qunar.com>
2017-07-11 19:12:13 +0800

    添加接口静态页面

--- client/ReduxContainer.js
+++ client/ReduxContainer.js
@@ -1,7 +1,9 @@
 import LoginRedux from './reducer/Login/Login_redux.js'
 import group from './reducer/group/group.js'
+import Interface from './reducer/interface/interface.js'
 
 export default {
+  group,
   LoginRedux,
-  group
+  Interface,
 }
