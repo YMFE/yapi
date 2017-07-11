ef337dc18d7cda15b493519c059f5c8f0db05e3a
zwjamnsss <zwjamnsss@gmail.com>
2017-07-11 16:35:39 +0800

    redux action

--- client/ReduxContainer.js
+++ client/ReduxContainer.js
@@ -1,5 +1,7 @@
 import LoginRedux from './reducer/Login/Login_redux.js'
+import group from './reducer/group/group.js'
 
 export default {
-  LoginRedux
+  LoginRedux,
+  group
 }
--- client/components/GroupList/GroupList.js
+++ client/components/GroupList/GroupList.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import { Card } from 'antd'
+
+import {
+  fetchGroupList,
+  fetchCurrGroup
+} from '../../actions/group.js'
+
 import './GroupList.scss'
 
+@connect(
+  state => ({
+    groupList: state.group.groupList,
+    currGroup: state.group.currGroup,
+  }),
+  {
+    fetchGroupList,
+    fetchCurrGroup
+  }
+)
 export default class GroupList extends Component {
   constructor(props) {
     super(props)
   }
 
-  
+  static propTypes = {
+    groupList: PropTypes.array,
+    currGroup: PropTypes.string
+  }
 
   render () {
+    const { groupList, currGroup } = this.props;
+
     return (
       <Card title="Groups">
-        <div>MFE</div>
-        <div>Hotel</div>
-        <div>Train</div>
-        <div>Flight</div>
-        <div>Pay</div>
-        <div>Vacation</div>
+        <div>{currGroup}</div>
+        {
+          groupList.map((group, index) => (
+            <div key={index}>{group}</div>
+          ))
+        }
       </Card>
     )
   }
