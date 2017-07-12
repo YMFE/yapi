8a29916edb01842e897bd1d861385071fcdbce32
zwjamnsss <zwjamnsss@gmail.com>
2017-07-12 14:06:29 +0800

    name

--- client/ReduxContainer.js
+++ client/ReduxContainer.js
@@ -1,6 +1,6 @@
 import LoginRedux from './reducer/Login/Login_redux.js'
 import group from './reducer/group/group.js'
-import Interface from './reducer/Interface/Interface.js'
+import Interface from './reducer/interface/interface.js'
 
 export default {
   group,
