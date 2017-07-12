a1452485591036dd26875f43d8967d909517913d
waliang.wang <waliang.wang@qunar.com>
2017-07-12 11:57:28 +0800

    优化reduce

--- client/ReduxContainer.js
+++ client/ReduxContainer.js
@@ -1,6 +1,6 @@
 import LoginRedux from './reducer/Login/Login_redux.js'
 import group from './reducer/group/group.js'
-import Interface from './reducer/interface/interface.js'
+import Interface from './reducer/Interface/Interface.js'
 
 export default {
   group,
