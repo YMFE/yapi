27c48c5ece9ee8ee1e11a266892404dc46a2e5dd
waliang.wang <waliang.wang@qunar.com>
2017-07-13 20:09:04 +0800

    管理项目成员模块

---
+++ client/containers/Interface/InterfaceMode/InterfaceMode.js
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+import { Modal, Input, Button } from 'antd'
+import PropTypes from 'prop-types'
+// import axios from 'axios'
+import ModeTag from './ModeTags'
+
+// Tags
+const ModeTags = () => {
+  const list = [1, 2, 3, 4]
+  return (
+    <article className="users">
+      <p>项目成员</p>
+      <div className="tags">
+        {
+          list.map((value, key) => {
+            return <ModeTag key={key} />
+          })
+        }
+      </div>
+    </article>
+  )
+}
+
+class InterfaceMode extends Component {
+  static propTypes = {
+    modalVisible: PropTypes.bool,
+    closeProjectMember: PropTypes.func,
+  }
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount () {
+    // axios.get({
+    //   url: '',
+    //   params: {}
+    // })
+    // .then(() => {
+
+    // })
+  }
+
+  handleOk (closeProjectMember) {
+    closeProjectMember()
+  }
+
+  handleCancel (closeProjectMember) {
+    closeProjectMember()
+  }
+
+  render() {
+    const { modalVisible, closeProjectMember } = this.props
+    const handleOk = this.handleOk.bind(this, closeProjectMember)
+    const handleCancel = this.handleCancel.bind(this, closeProjectMember)
+
+    return (
+      <Modal
+        title="项目成员管理"
+        onOk={ handleOk }
+        visible={ modalVisible }
+        onCancel={ handleCancel }
+        className="interface-mode-box"
+      >
+        <div className="add-user">
+          <Input placeholder="Basic usage" size="large" />
+          <Button>添加新成员</Button>          
+        </div>
+        
+        <ModeTags />
+      </Modal>
+    )
+  }
+}
+
+export default InterfaceMode
---
+++ client/reducer/Interface/InterfaceReducer.js
@@ -0,0 +1,32 @@
+import {
+  FETCH_INTERFACE_DATA,
+  LIST_INTERFACE_CLICK,
+  PROJECT_MEMBER_INTERFACE,
+} from '../../constants/action-types.js'
+
+const initialState = {
+  interfaceData: null,
+  modalVisible: false,
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_INTERFACE_DATA:
+      return {
+        ...state,
+        interfaceData: action.payload,
+      }
+    case LIST_INTERFACE_CLICK:
+      return {
+        ...state,
+        modalVisible: true,
+      }
+    case PROJECT_MEMBER_INTERFACE:
+      return {
+        ...state,
+        modalVisible: false,
+      }
+    default:
+      return state
+  }
+}
