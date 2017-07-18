0f8cfdae79b25aa45e34bb037255033e3cd74ec8
suxiaoxin <hellosean1025@163.com>
2017-07-18 19:47:38 +0800

    feat: user client module

--- client/containers/User/List.js
+++ client/containers/User/List.js
@@ -1,47 +1,14 @@
 import React, { Component } from 'react'
-import {formatTime} from '../../common.js'
+import { formatTime } from '../../common.js'
 import { Link } from 'react-router-dom'
 //import PropTypes from 'prop-types'
 import {
   Table,
-  Button
+  Button,
+  Popconfirm,
+  message
 } from 'antd'
 import axios from 'axios';
-
-
-
-const columns = [{
-  title: 'UID',
-  dataIndex: '_id',
-  key: '_id'
-}, {
-  title: '用户名',
-  dataIndex: 'username',
-  key: 'username'
-}, {
-  title: 'Email',
-  dataIndex: 'email',
-  key: 'email'
-}, {
-  title: '用户角色',
-  dataIndex: 'role',
-  key: 'role'
-}, {
-  title: '更新日期',
-  dataIndex: 'up_time',
-  key: 'up_time'
-}, {
-  title: '功能',
-  key: 'action',
-  render: (item) => {
-    return (
-      <span>
-        <Button type="primary"><Link to={"/user/profile/"+item._id} > 查看 </Link></Button>
-        <Button type="danger">删除</Button>
-      </span>
-    )
-  }
-}]
 
 class List extends Component {
 
@@ -56,12 +23,12 @@ class List extends Component {
     axios.get('/user/list').then((res) => {
       let result = res.data;
 
-      if (result.errcode === 0) {    
+      if (result.errcode === 0) {
         let list = result.data.list;
-        list.map( (item, index) => {
+        list.map((item, index) => {
           item.key = index;
           item.up_time = formatTime(item.up_time)
-        } )
+        })
         this.setState({
           data: list
         });
@@ -73,9 +40,71 @@ class List extends Component {
     this.getUserList()
   }
 
-  render() {
+  confirm = (uid) =>{
+    axios.post('/user/del', {
+      id: uid
+    }).then( (res)=>{
+      if(res.data.errcode === 0){
+        message.success('已删除此用户');
+        let userlist = this.state.data;
+        userlist = userlist.filter( (item)=>{
+          return item._id != uid
+        } )
+        this.setState({
+          data: userlist
+        })
+      }else{
+        message.error(res.data.errmsg);
+      }
+    }, (err) => {
+      message.error(err.message);
+    } )
+  }
 
+  render() {
+    const role = 'admin'
     const data = this.state.data;
+    let columns = [{
+      title: 'UID',
+      dataIndex: '_id',
+      key: '_id'
+    }, {
+      title: '用户名',
+      dataIndex: 'username',
+      key: 'username'
+    }, {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email'
+    }, {
+      title: '用户角色',
+      dataIndex: 'role',
+      key: 'role'
+    }, {
+      title: '更新日期',
+      dataIndex: 'up_time',
+      key: 'up_time'
+    }, {
+      title: '功能',
+      key: 'action',
+      render: (item) => {
+        return (
+          <span>            
+            <Button type="primary"><Link to={"/user/profile/" + item._id} > 查看 </Link></Button>
+            <Popconfirm placement="leftTop" title="Are you sure delete this task?"  onConfirm={() => {this.confirm(item._id)}} okText="Yes" cancelText="No">
+              <Button type="danger">删除</Button>
+            </Popconfirm>
+          </span>
+        )
+      }
+    }]
+
+    columns = columns.filter( (item)=>{
+      if(item.key === 'action' && role !== 'admin'){
+        return false;
+      }
+      return true;
+    } )
 
     return (
       <section className="user-table">
