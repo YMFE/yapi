6fe68c00acaf1317b6d8274f30bdbbeb31e7156b
waliang.wang <waliang.wang@qunar.com>
2017-07-18 12:00:36 +0800

    合并冲突

---
+++ client/reducer/login/login.js
@@ -0,0 +1,32 @@
+import {
+  LOGIN,
+  LOGIN_TYPE
+} from '../../constants/action-types';
+
+const initialState = {
+  isLogin: false,
+  userName: null,
+  uid: null,
+  loginWrapActiveKey:"1"
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case LOGIN: {
+      console.log(action);
+      return {
+        ...state,
+        isLogin: true,
+        userName: action.payload.data.userName
+      };
+    }
+    case LOGIN_TYPE: {
+      return {
+        ...state,
+        loginWrapActiveKey: action.index
+      };
+    }
+    default:
+      return state;
+  }
+};
