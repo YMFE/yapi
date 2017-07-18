708e75a3b2f2cbdabca088f5dab1ac606c8b026f
waliang.wang <waliang.wang@qunar.com>
2017-07-18 11:57:42 +0800

    解决冲突

--- client/ReduxContainer.js
+++ client/ReduxContainer.js
@@ -2,7 +2,7 @@ import LoginRedux from './reducer/Login/Login_redux.js'
 import login from './reducer/Login/login.js'
 import group from './reducer/group/group.js'
 import project from './reducer/group/project.js'
-import Interface from './reducer/Interface/InterfaceReducer.js'
+import Interface from './reducer/interface/InterfaceReducer.js'
 import news from './reducer/news/news.js'
 
 export default {
