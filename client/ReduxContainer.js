b96a2a3de993c03f15c94be10a129b0dc10d9311
wenbo.dong <wenbo.dong@qunar.com>
2017-07-18 10:56:03 +0800

    feat: 添加项目逻辑

--- client/ReduxContainer.js
+++ client/ReduxContainer.js
@@ -1,11 +1,13 @@
 import LoginRedux from './reducer/Login/Login_redux.js'
 import login from './reducer/Login/login.js'
 import group from './reducer/group/group.js'
+import project from './reducer/group/project.js'
 import Interface from './reducer/Interface/InterfaceReducer.js'
 
 export default {
   group,
   login,
   LoginRedux,
-  Interface
+  Interface,
+  project
 }
