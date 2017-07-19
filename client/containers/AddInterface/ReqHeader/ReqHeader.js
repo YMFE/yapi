19e97e9b67043c55dfef35eba15f012ffe613364
waliang.wang <waliang.wang@qunar.com>
2017-07-19 15:12:10 +0800

    添加 删除 接口

--- client/containers/AddInterface/ReqHeader/ReqHeader.js
+++ client/containers/AddInterface/ReqHeader/ReqHeader.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react'
 import { Button } from 'antd'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { autobind } from 'core-decorators'
 import ReqList from './ReqList.js'
+import { 
+  addReqHeader
+} from '../../../actions/addInterface.js'
+
+// 重新渲染页面
+const getReqList = function (self) {
+  const [reqList, seqGroup] = [[], self.props.seqGroup]
+  seqGroup.map((value, key) => {
+    reqList.push(<ReqList key={key} dataNum={value.id} />)
+  })
+  return reqList
+}
+
+@connect(
+  state => {
+    return {
+      seqGroup: state.addInterface.seqGroup
+    }
+  },
+  {
+    addReqHeader
+  }
+)
 
 class ReqHeader extends Component {
+  static propTypes = {
+    seqGroup: PropTypes.array,
+    addReqHeader: PropTypes.func
+  }
+
   constructor(props) {
     super(props)
+  }
+
+  @autobind
+  addSeqGroup () {
+    let newSeqGroup = []
+    let seqGroup = this.props.seqGroup
+    let id = seqGroup[seqGroup.length-1].id    
+    let list = {
+      id: ++id,
+      tag: '',
+      content: ''
+    }
+    seqGroup.push(list)
+    newSeqGroup.push(...seqGroup)
+    this.props.addReqHeader(newSeqGroup)
   }
 
   render () {
@@ -13,10 +59,10 @@ class ReqHeader extends Component {
         <div className="req-header">
           <strong className="req-h3">请求头部 :</strong>
           <ul>
-            <ReqList />
+            { getReqList(this) }
           </ul>
         </div>
-        <Button type="primary" className="req-save">添加</Button>
+        <Button type="primary" className="req-save" onClick={this.addSeqGroup}>添加</Button>
       </section>
     )
   }
--- client/containers/AddInterface/ReqParams/ReqParams.js
+++ client/containers/AddInterface/ReqParams/ReqParams.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
 import { Button } from 'antd'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import ParamsList from './ParamsList.js'
+import { autobind } from 'core-decorators'
+import { 
+  addReqParams
+} from '../../../actions/addInterface.js'
+
+@connect(
+  state => {
+    return {
+      seqParams: state.addInterface.seqParams
+    }
+  },
+  {
+    addReqParams
+  }
+)
 
 class ReqParams extends Component {
+  static propTypes = {
+    addReqParams: PropTypes.func,
+    seqParams: PropTypes.array
+  }
+
   constructor(props) {
     super(props)
+  }
+
+  @autobind
+  addSeqParams () {
+    let newSeqParams= []
+    let seqParams = this.props.seqParams
+    let id = seqParams[seqParams.length-1].id
+    let list = {
+      id: ++id,
+      paramsName: '',
+      describe: ''
+    }
+    seqParams.push(list)
+    newSeqParams.push(...seqParams)
+    // this.props.addReqParams(newSeqParams)
   }
 
   render () {
@@ -17,7 +54,7 @@ class ReqParams extends Component {
           </ul>
         </div>
 
-        <Button type="primary" className="req-save">添加</Button>
+        <Button type="primary" className="req-save" onClick={this.addSeqParams}>添加</Button>
       </section>
     )
   }
