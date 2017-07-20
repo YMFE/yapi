c1683d2ae03bcf4ae3a0b5caa50bcc8f8401a8ab
wenbo.dong <wenbo.dong@qunar.com>
2017-07-20 10:51:31 +0800

    docs: 修改login-wrap文件名

--- client/containers/index.js
+++ client/containers/index.js
@@ -1,5 +1,5 @@
 import Home from './Home/Home.js'
-import Login from './Login/Login-wrap.js'
+import Login from './Login/LoginWrap.js'
 import ProjectGroups from './ProjectGroups/ProjectGroups.js'
 import Interface from './Interface/Interface.js'
 import News from './News/News.js'
