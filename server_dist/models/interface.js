70a9660db3df136743267778898173b58b3774df
sean <wenxiong.su@qunar.com>
2017-07-20 16:44:41 +0800

    feat: user style

--- server_dist/models/interface.js
+++ server_dist/models/interface.js
@@ -47,13 +47,9 @@ var interfaceModel = function (_baseModel) {
         key: 'getSchema',
         value: function getSchema() {
             return {
+                title: { type: String, required: true },
                 uid: { type: Number, required: true },
-                path: { type: String, required: true, validate: {
-                        validator: function validator(v) {
-                            return v && v[0] !== '/';
-                        },
-                        message: '接口路径第一位不能是/'
-                    } },
+                path: { type: String, required: true },
                 method: { type: String, required: true },
                 project_id: { type: Number, required: true },
                 desc: String,
