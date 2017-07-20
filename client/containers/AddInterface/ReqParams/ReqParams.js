f34d5af35bb3da6d8c5fa8475d0c50584f4c173b
waliang.wang <waliang.wang@qunar.com>
2017-07-20 15:38:25 +0800

    feat - 接口列表数据

--- client/containers/AddInterface/ReqParams/ReqParams.js
+++ client/containers/AddInterface/ReqParams/ReqParams.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-// import { autobind } from 'core-decorators'
 import wangEditor from 'wangeditor'
 import { getReqParams } from '../../../actions/addInterface.js'
 
