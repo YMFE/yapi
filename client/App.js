5c9914f3673df0d61fc4e14f29f76bd84d369c5e
yhui.yang <qitmac000068@qitmac000068deMacBook-Pro.local>
2017-07-20 19:59:49 +0800

    feat: 添加header上的搜索，修改路由，修改登录页面样式

--- client/App.js
+++ client/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
-import axios from 'axios';
-import { Route, HashRouter, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
+import { Route, HashRouter, Redirect, Switch } from 'react-router-dom'
 import { Home, ProjectGroups, Interface, News } from './containers/index'
 import User from './containers/User/User.js'
 import Header from './components/Header/Header'
+import { checkLoginState } from './actions/login'
+
 
 const LOADING_STATUS = 0;
 const GUEST_STATUS = 1;
@@ -12,12 +15,14 @@ const MEMBER_STATUS = 2;
 class App extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       login: LOADING_STATUS
     }
   }
-
+  static propTypes = {
+    checkLoginState:PropTypes.func
+  }
   route = (status) => {
     let r;
     if (status === LOADING_STATUS) {
@@ -25,14 +30,16 @@ class App extends Component {
     } else if (status === GUEST_STATUS) {
       r = (
         <HashRouter>
-
           <div className="router-main">
             <Header />
             <Route path="/" component={Home} exact />
-            <Redirect to="/" />
+            <Switch>
+              <Route
+                path="/"
+                component={Home}/>
+              <Redirect from="(/:str)" to="/" />
+            </Switch>
           </div>
-
-
         </HashRouter>
       )
     } else {
@@ -40,13 +47,12 @@ class App extends Component {
         <HashRouter>
           <div className="router-main">
             <Header />
-            <Route path="/" component={ProjectGroups} exact />
+            <Route path="/" component={Home} exact />
             <Route path="/ProjectGroups" component={ProjectGroups} />
             <Route path="/Interface" component={Interface} />
             <Route path="/user" component={User} />
             <Route path="/News" component={News} />
           </div>
-
         </HashRouter>
       )
     }
@@ -55,9 +61,9 @@ class App extends Component {
   }
 
   componentDidMount() {
-    console.log('app.js init')
-    axios.get('/user/status').then((res) => {
-      if (res.data.errcode === 0 && res.data.data._id > 0) {
+    this.props.checkLoginState().then((res) => {
+      console.log(res);
+      if (res.payload.data.errcode === 0 && res.payload.data.data._id > 0) {
         this.setState({
           login: MEMBER_STATUS
         })
@@ -66,22 +72,26 @@ class App extends Component {
           login: GUEST_STATUS
         })
       }
-    }, (err) => {
+    }).catch((err) => {
       this.setState({
         login: GUEST_STATUS
-      })
-      console.log(err.message)
-    })
-
+      });
+      console.log(err)
+    });
   }
 
   render() {
-    console.log(MEMBER_STATUS)
-    console.log(this.route(this.state.login))
     return this.route(this.state.login)
-
-
   }
 }
 
-export default App
+export default connect(
+  state => {
+    return{
+      login:state.login.isLogin
+    }
+  },
+  {
+    checkLoginState
+  }
+)(App)
