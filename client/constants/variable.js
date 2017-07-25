62bf3bd22eaba03c92d476674efe156ec2feebfc
wenbo.dong <wenbo.dong@qunar.com>
2017-07-25 11:22:42 +0800

    feat: 登录后跳转到首页

--- client/constants/variable.js
+++ client/constants/variable.js
@@ -2,6 +2,6 @@ export default {
   PAGE_LIMIT: 10, // 默认每页展示10条数据
 
   // layout
-  ROW_MIN_WIDTH: '9.7rem',
-  ROW_MAX_WIDTH: '11.7rem'
+  ROW_MIN_WIDTH: '9.7rem', // 适应小屏幕分辨率
+  ROW_MAX_WIDTH: '11.7rem' // 适应大屏幕分辨率
 }
