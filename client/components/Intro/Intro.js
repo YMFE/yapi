50aa0d1bec312f6dd3a364a827d49809081a79b1
yhui.yang <qitmac000068@qitmac000068deMacBook-Pro.local>
2017-07-25 15:35:23 +0800

    opti: 首页添加动画，修改header

--- client/components/Intro/Intro.js
+++ client/components/Intro/Intro.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Row, Col, Icon } from 'antd'
+import { Col, Icon } from 'antd'
 import "./Intro.scss"
+import { OverPack } from 'rc-scroll-anim'
+import TweenOne from 'rc-tween-one'
+import QueueAnim from 'rc-queue-anim';
 
 const IntroPart = (props) =>(
   <Col span={12} className="switch-content">
@@ -33,22 +36,42 @@ class Intro extends React.Component{
         title:PropTypes.string,
         des:PropTypes.string
       }))
-    })
+    }),
+    className : PropTypes.string
   }
   render(){
-    const { intro } = this.props;
+    const {  intro } = this.props;
+    const id = "motion";
+    const animType = {
+      queue: 'right',
+      one: { x: '-=30', opacity: 0, type: 'from' }
+    };
     return(
       <div className="intro-container">
-        <Row>
-          <Col span={12}>
-            <div>
-              <div className="img-container">
-                <img src={intro.img}/>
-              </div>
+        <OverPack
+          playScale="0.3"
+        >
+          <TweenOne
+            animation={animType.one}
+            key={`${id}-img`}
+            resetStyleBool
+            id={`${id}-imgWrapper`}
+            className="imgWrapper"
+          >
+            <div className="img-container" id={`${id}-img-container`}>
+              <img src={intro.img}/>
             </div>
-          </Col>
-          <Col span={12} className="des-container">
-            <div>
+          </TweenOne>
+
+          <QueueAnim
+            type={animType.queue}
+            key={`${id}-text`}
+            leaveReverse
+            ease={['easeOutCubic', 'easeInCubic']}
+            id={`${id}-textWrapper`}
+            className={`${id}-text des-container textWrapper`}
+          >
+            <div key={`${id}-des-content`}>
               <div className="des-title">
                 {intro.title}
               </div>
@@ -56,13 +79,13 @@ class Intro extends React.Component{
                 {intro.des}
               </div>
             </div>
-            <div className="des-switch">
+            <div className="des-switch" key={`${id}-des-switch`}>
               {intro.detail.map(function(item,i){
                 return(<IntroPart key={i} title={item.title} des={item.des}/>)
               })}
             </div>
-          </Col>
-        </Row>
+          </QueueAnim>
+        </OverPack>
       </div>
     )
   }
