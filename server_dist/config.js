4c1eff6404b1d284616ca73d4af2f356a63e4932
suxiaoxin <hellosean1025@163.com>
2017-07-27 12:03:18 +0800

    chore: ops

--- server_dist/config.js
+++ server_dist/config.js
@@ -1,18 +1,11 @@
-'use strict';
-
-var _path = require('path');
-
-var _path2 = _interopRequireDefault(_path);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+"use strict";
 
 /**
  * config.js是用来第一次安装初始化网站配置，如果不用默认的runtime_path，可以直接修改runtime_path路径
  */
-var runtime_path = _path2.default.join(_path2.default.resolve(__dirname, '../'), 'runtime');
 var config = {
   "port": 80,
-  "runtime_path": runtime_path,
+  "runtime_path": '',
   "webhost": "yapi.local.qunar.com",
   "adminAccount": "admin@admin.com",
   "db": {
