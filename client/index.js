418e6647bce106e99111323afcf0dc48856c41c4
zwjamnsss <zwjamnsss@gmail.com>
2017-08-04 12:15:45 +0800

    feat: add redux-devtools

--- client/index.js
+++ client/index.js
@@ -4,19 +4,30 @@ import thunkMiddleware from 'redux-thunk'
 import promiseMiddleware from 'redux-promise';
 import ReactDOM from 'react-dom'
 import App from './Application'
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
 import ReduxContainer from './ReduxContainer.js'
+import { DevTools } from './containers';
+
+const enhancer = compose(
+  // Middleware you want to use in development:
+  applyMiddleware(
+    thunkMiddleware.default,
+    promiseMiddleware
+  ),
+  // Required! Enable Redux DevTools with the monitors you chose
+  DevTools.instrument()
+);
 
 // 合并 redux 创建stroe
-const store = createStore(combineReducers( ReduxContainer ), applyMiddleware(
-  thunkMiddleware.default,
-  promiseMiddleware
-))
+const store = createStore(combineReducers( ReduxContainer ), {}, enhancer)
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <div>
+      <App />
+      <DevTools />
+    </div>
   </Provider>,
   document.getElementById('yapi')
 )
