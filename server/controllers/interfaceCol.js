ef0475b66c4e9a55f4e7d86312693ff0e138795d
sean <wenxiong.su@qunar.com>
2017-08-08 19:47:36 +0800

    chore: client dev env path

--- server/controllers/interfaceCol.js
+++ server/controllers/interfaceCol.js
@@ -8,16 +8,23 @@ class interfaceColController extends baseController{
         this.Model = yapi.getInst(interfaceColModel);
     }
 
-    list(ctx){
-
+    async list(ctx){
+        try {
+            let id = ctx.query.project_id;
+            let inst = yapi.getInst(interfaceColModel);
+            let result = await inst.list(id);
+            ctx.body = yapi.commons.resReturn(result);
+        } catch (e) {
+            ctx.body = yapi.commons.resReturn(null, 402, e.message);
+        }
     }
 
-    up(ctx){
+    async up(ctx){
         
     }
 
-    del(ctx){
-
+    async del(ctx){
+        
     }
 
 
