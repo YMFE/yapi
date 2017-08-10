3d66d5389b4a0d8e8af1cbe10fa7dc73fff396f8
zwjamnsss <zwjamnsss@gmail.com>
2017-08-10 16:00:52 +0800

    opti: 晚上目录结构

--- client/containers/index.js
+++ client/containers/index.js
@@ -1,8 +1,9 @@
 import Header from '../components/Header/Header.js'
 import Home from './Home/Home.js'
 import Login from './Login/LoginWrap.js'
-import ProjectGroups from './ProjectGroups/ProjectGroups.js'
+import Group from './Group/Group.js'
 import Interface from './Interface/Interface.js'
+import Project from './Project/Project.js'
 import News from './News/News.js'
 import AddInterface from './AddInterface/AddInterface.js'
 import DevTools from './DevTools/DevTools.js'
@@ -12,8 +13,9 @@ export {
   Header,
   Home,
   Login,
-  ProjectGroups,
+  Group,
   Interface,
+  Project,
   AddInterface,
   News,
   DevTools,
