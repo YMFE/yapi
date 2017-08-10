20971295f0a87ed1d3e4e2faa6a685a1279e2fba
zwjamnsss <zwjamnsss@gmail.com>
2017-08-10 20:56:08 +0800

    feat: 项目路由

--- client/containers/Project/Project.js
+++ client/containers/Project/Project.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { Subnav } from '../../components/index'
 
 export default class GroupList extends Component {
 
@@ -21,8 +22,20 @@ export default class GroupList extends Component {
   render () {
     return (
       <div>
-        我是项目页喔
+        <Subnav
+          default={'接口'}
+          data={[{
+            name: '接口',
+            path: '/project/:id/activity'
+          }, {
+            name: '设置',
+            path: '/project/:id/setting'
+          }, {
+            name: '动态',
+            path: '/project/:id/activity'
+          }]}/>
         <Switch>
+          <Redirect exact from='/project/:id' to='/project/:id/interface' />
           <Route path="/project/:id/activity" component={null} />
           <Route path="/project/:id/interface" component={null} />
           <Route path="/project/:id/setting" component={null} />
