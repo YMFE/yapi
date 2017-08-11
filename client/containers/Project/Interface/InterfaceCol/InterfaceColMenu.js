56f3b6e3766a0b5074fb36d5f803ec857a6a20a9
zwjamnsss <zwjamnsss@gmail.com>
2017-08-11 21:10:54 +0800

    feat: 接口集合

--- client/containers/Project/Interface/InterfaceCol/InterfaceColMenu.js
+++ client/containers/Project/Interface/InterfaceCol/InterfaceColMenu.js
@@ -1,4 +1,10 @@
-import React from 'react'
-export default () => {
-  return <h1>hello colContent</h1>
+import React, { Component } from 'react'
+export default class InterfaceColMenu extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <h1>hello colContent</h1>
+  }
 }
