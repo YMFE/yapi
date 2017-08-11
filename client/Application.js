8bdbf092e6aaa1ef34d5ef5744c9a9d524423e5e
zwjamnsss <zwjamnsss@gmail.com>
2017-08-11 15:02:07 +0800

    opti: 路由

--- client/Application.js
+++ client/Application.js
@@ -56,9 +56,9 @@ export default class App extends Component {
           <div className="router-main">
             <Header />
             <div className="router-container">
-              <Route path="/" component={Home} exact />
+              <Route exact path="/" component={Home} />
               <Route path="/group" component={requireAuthentication(Group)} />
-              <Route path="/project/:id" component={requireAuthentication(Project)} />
+              <Route path="/project" component={requireAuthentication(Project)} />
               <Route path="/user" component={requireAuthentication(User)} />
               <Route path="/follow" component={requireAuthentication(Follows)} />
               <Route path="/add-project" component={requireAuthentication(AddProject)} />
