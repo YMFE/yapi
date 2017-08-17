8b4327a4424c9bdb0bf363c3a3526c8db1a448b5
wenbo.dong <wenbo.dong@qunar.com>
2017-08-17 12:09:08 +0800

    feat: 空项目组提示

---
+++ client/containers/Project/Setting/Setting.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Setting extends Component {
+  render () {
+    return (
+      <div>
+        <div className="g-row">
+          <section className="news-box">
+            t额st
+          </section>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Setting
