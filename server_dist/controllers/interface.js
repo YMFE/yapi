6268c37ef20ceac4ebd773533a928e1c989cf4f0
qitmac000249 <xilli.yu@Qunar.com>
2017-08-24 16:40:33 +0800

    fix: modify

--- server_dist/controllers/interface.js
+++ server_dist/controllers/interface.js
@@ -105,7 +105,7 @@ var interfaceController = function (_baseController) {
     (0, _createClass3.default)(interfaceController, [{
         key: 'add',
         value: function () {
-            var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(ctx) {
+            var _ref = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee(ctx) {
                 var params, auth, checkRepeat, data, result, username, cate;
                 return _regenerator2.default.wrap(function _callee$(_context) {
                     while (1) {
@@ -273,7 +273,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'get',
         value: function () {
-            var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(ctx) {
+            var _ref2 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee2(ctx) {
                 var params, result;
                 return _regenerator2.default.wrap(function _callee2$(_context2) {
                     while (1) {
@@ -335,7 +335,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'list',
         value: function () {
-            var _ref3 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee3(ctx) {
+            var _ref3 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee3(ctx) {
                 var project_id, result;
                 return _regenerator2.default.wrap(function _callee3$(_context3) {
                     while (1) {
@@ -385,7 +385,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'listByCat',
         value: function () {
-            var _ref4 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee4(ctx) {
+            var _ref4 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee4(ctx) {
                 var catid, result;
                 return _regenerator2.default.wrap(function _callee4$(_context4) {
                     while (1) {
@@ -435,7 +435,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'listByMenu',
         value: function () {
-            var _ref5 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee5(ctx) {
+            var _ref5 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee5(ctx) {
                 var project_id, result, newResult, i, item, list, j;
                 return _regenerator2.default.wrap(function _callee5$(_context5) {
                     while (1) {
@@ -540,7 +540,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'up',
         value: function () {
-            var _ref6 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee6(ctx) {
+            var _ref6 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee6(ctx) {
                 var params, id, interfaceData, auth, checkRepeat, data, result, username, cate, inter;
                 return _regenerator2.default.wrap(function _callee6$(_context6) {
                     while (1) {
@@ -753,7 +753,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'del',
         value: function () {
-            var _ref7 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee7(ctx) {
+            var _ref7 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee7(ctx) {
                 var id, data, auth, inter, result, username, cate;
                 return _regenerator2.default.wrap(function _callee7$(_context7) {
                     while (1) {
@@ -847,7 +847,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'solveConflict',
         value: function () {
-            var _ref8 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee8(ctx) {
+            var _ref8 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee8(ctx) {
                 var _this2 = this;
 
                 var id, result, userInst, userinfo, data;
@@ -930,7 +930,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'addCat',
         value: function () {
-            var _ref9 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee9(ctx) {
+            var _ref9 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee9(ctx) {
                 var params, auth, result, username;
                 return _regenerator2.default.wrap(function _callee9$(_context9) {
                     while (1) {
@@ -1025,7 +1025,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'upCat',
         value: function () {
-            var _ref10 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee10(ctx) {
+            var _ref10 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee10(ctx) {
                 var params, result, username, cate, auth;
                 return _regenerator2.default.wrap(function _callee10$(_context10) {
                     while (1) {
@@ -1097,7 +1097,7 @@ var interfaceController = function (_baseController) {
     }, {
         key: 'delCat',
         value: function () {
-            var _ref11 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee11(ctx) {
+            var _ref11 = (0, _asyncToGenerator3.default)(_regenerator2.default.mark(function _callee11(ctx) {
                 var id, catData, auth, cate, result, r, username;
                 return _regenerator2.default.wrap(function _callee11$(_context11) {
                     while (1) {
