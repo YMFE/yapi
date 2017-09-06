a39525500a3b5c69696a9d2f04d08d4d7fd246b0
sean <wenxiong.su@qunar.com>
2017-09-06 10:19:48 +0800

    chore: update fe build

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@eab742fd3c28ea24fc95.js","css":"index@eab742fd3c28ea24fc95.css"},"lib":{"js":"lib@3c3db91cbdc265b7c37f.js"},"lib2":{"js":"lib2@122f3773fc11dd227117.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@cfc6b632656fd864f0e9.js","css":"index@cfc6b632656fd864f0e9.css"},"lib":{"js":"lib@f5d23a2a5e455cd73eb0.js"},"lib2":{"js":"lib2@122f3773fc11dd227117.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
