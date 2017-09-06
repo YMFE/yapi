d346a72a85bb94b0b92f06999158beac3deb6456
喻希里 <xilli.yu@Qunar.com>
2017-09-06 15:49:47 +0800

    fix: 修复接口预览页的req_body有时不显示问题

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@cfc6b632656fd864f0e9.js","css":"index@cfc6b632656fd864f0e9.css"},"lib":{"js":"lib@f5d23a2a5e455cd73eb0.js"},"lib2":{"js":"lib2@122f3773fc11dd227117.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@1f9686ea53bc4f0c765a.js","css":"index@1f9686ea53bc4f0c765a.css"},"lib":{"js":"lib@37aefd9cc41e6ef59eba.js"},"lib2":{"js":"lib2@122f3773fc11dd227117.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
