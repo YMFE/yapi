18580a92f4a4d941b5e1cf77b074e55ef95d787d
suxiaoxin <hellosean1025@163.com>
2017-09-21 20:47:08 +0800

    opti: interface Edit style

--- exts/yapi-plugin-advanced-mock/AdvMock.js
+++ exts/yapi-plugin-advanced-mock/AdvMock.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
 import { withRouter } from 'react-router-dom';
-import { Form, Switch, Button, message, Icon } from 'antd';
+import { Form, Switch, Button, message, Icon, Tooltip } from 'antd';
 import mockEditor from 'client/containers/Project/Interface/InterfaceList/mockEditor';
 const FormItem = Form.Item;
 
@@ -95,7 +95,7 @@ class AdvMock extends Component {
       
       <Form onSubmit={this.handleSubmit}>
         <FormItem
-          label={<span>是否开启&nbsp;<a target="_blank" rel="noopener noreferrer" title="点击查看文档"  href="https://yapi.ymfe.org/mock.html#高级Mock" ><Icon type="question-circle-o" /></a></span>}
+          label={<span>是否开启&nbsp;<a target="_blank" rel="noopener noreferrer"   href="https://yapi.ymfe.org/mock.html#高级Mock" ><Tooltip title="点击查看文档"><Icon type="question-circle-o" /></Tooltip></a></span>}
           {...formItemLayout}
         >
           <Switch checked={this.state.enable} onChange={this.onChange} checkedChildren="开" unCheckedChildren="关" />
