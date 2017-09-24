8f8bca9d7c0598babcadb4cc59e61e529e141fed
suxiaoxin <hellosean1025@163.com>
2017-09-25 00:07:23 +0800

    opti: plugin

--- server/plugin.js
+++ server/plugin.js
@@ -3,36 +3,102 @@ const plugin_path = yapi.path.join(yapi.WEBROOT, 'plugins');
 const plugin_system_path = yapi.path.join(yapi.WEBROOT, 'exts');
 const initPlugins = require('../common/lib.js').initPlugins;
 var extConfig = require('../common/config.js').exts;
+
+/**
+ * 钩子配置
+ */
 var hooks = {
-    //第三方sso登录钩子，暂只支持设置一个
+    /**
+     * 第三方sso登录钩子，暂只支持设置一个
+     * @param ctx 
+     */
     'third_login': {
         type: 'single',
         listener: null
     },
-    //增加接口成功后触发
+    /**
+     * 客户端增加接口成功后触发
+     * @param id 接口id
+     */
     'interface_add': {
         type: 'multi',
         listener: []
     },
-    //删除接口成功后触发
+    /**
+     * 客户端删除接口成功后触发
+     * @param id 接口id
+     */
     'interface_del': {
         type: 'multi',
         listener: []
     },
+    /**
+    * 客户端更新接口成功后触发
+    * @param id 接口id
+    */
+    'interface_update':{
+        type: 'multi',
+        listener: []
+    },
+    /**
+     * 客户端获取接口数据列表
+     * @param id project_id
+     */
+    'interface_list':{
+        type: 'multi',
+        listener: []
+    },
+    /**
+     * 客户端获取一条接口信息触发
+     * @param id 接口id
+     */
+    'interface_get':{
+        type: 'multi',
+        listener: []
+    },
+    /**
+     * 客户端增加一个新项目
+     * @param id 项目id
+     */
     'project_add':{
         type: 'multi',
         listener: []
     },
+    /**
+     * 客户端删除删除一个项目
+     * @param id 项目id
+     */
     'project_del':{
         type: 'multi',
         listener: []
     },
-    //MockServer生成mock数据后触发
+    /**
+     * MockServer生成mock数据后触发
+     * @param context Object
+     * {
+     *  projectData: project,
+        interfaceData: interfaceData,
+        ctx: ctx,
+        mockJson: res 
+     * }
+     * 
+     */
     mock_after: {
         type: 'multi',
         listener: []
     },
-    //增加路由的钩子
+    /**
+     * 增加路由的钩子
+     * type Sync
+     * @param addPluginRouter Function 
+     * addPLuginPLugin(config)
+     * config = {
+     *  path,      // String
+     *  method,    // String
+     *  controller // Class 继承baseController的class
+     *  action     // String controller的Action
+     * }
+     */
     add_router: {
         type: 'multi',
         listener: []
