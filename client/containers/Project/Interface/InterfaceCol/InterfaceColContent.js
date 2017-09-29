35494776c36cd45bf517a0b8d9e8567841311da0
suxiaoxin <hellosean1025@163.com>
2017-09-29 18:10:54 +0800

    opti: 没有安装插件disable开始测试

--- client/containers/Project/Interface/InterfaceCol/InterfaceColContent.js
+++ client/containers/Project/Interface/InterfaceCol/InterfaceColContent.js
@@ -72,7 +72,8 @@ class InterfaceColContent extends Component {
       rows: [],
       reports: {},
       visible: false,
-      curCaseid: null
+      curCaseid: null,
+      hasPlugin: false
     };
     this.onRow = this.onRow.bind(this);
     this.onMoveRow = this.onMoveRow.bind(this);
@@ -93,6 +94,29 @@ class InterfaceColContent extends Component {
       this.handleColdata(this.props.currCaseList)
     }
 
+    let startTime = 0;
+    this.interval = setInterval(() => {
+      startTime += 500;
+      if (startTime > 5000) {
+        clearInterval(this.interval);
+      }
+      if (window.crossRequest) {
+        clearInterval(this.interval);
+        this.setState({
+          hasPlugin: true
+        })
+      } else {
+        this.setState({
+          hasPlugin: false
+        })
+      }
+    }, 500)
+    this.getInterfaceState()
+
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval)
   }
 
   handleColdata = (rows) => {
@@ -187,7 +211,7 @@ class InterfaceColContent extends Component {
             })
 
             let validRes = [];
-            if(interfaceData.mock_verify){
+            if (interfaceData.mock_verify) {
               validRes = Mock.valid(tpl, res);
             }
             if (validRes.length === 0) {
@@ -447,7 +471,13 @@ class InterfaceColContent extends Component {
         <h2 className="interface-title" style={{ display: 'inline-block', margin: 0, marginBottom: '16px' }}>测试集合&nbsp;<a target="_blank" rel="noopener noreferrer" href="https://yapi.ymfe.org/case.html" >
           <Tooltip title="点击查看文档"><Icon type="question-circle-o" /></Tooltip>
         </a></h2>
-        <Button type="primary" style={{ float: 'right' }} onClick={this.executeTests}>开始测试</Button>
+        {this.state.hasPlugin?
+          <Button type="primary" style={{ float: 'right' }} onClick={this.executeTests}>开始测试</Button>:
+          <Tooltip title="请安装 cross-request Chrome 插件">
+            <Button disabled type="primary" style={{ float: 'right' }} >开始测试</Button>
+          </Tooltip>
+        }
+        
         <Table.Provider
           components={components}
           columns={resolvedColumns}
