40bf71108719eeb18d96be19a36aa86aedb56d51
suxiaoxin <hellosean1025@163.com>
2017-10-11 17:53:29 +0800

    fix: del console code

--- client/containers/Project/Interface/InterfaceList/InterfaceEditForm.js
+++ client/containers/Project/Interface/InterfaceList/InterfaceEditForm.js
@@ -241,8 +241,6 @@ class InterfaceEditForm extends Component {
       readOnly: true
     })
 
-    mockPreview.setValue('bbbb')
-
     let editor = this.editor = new Editor('#desc');
     editor.create();
     editor.txt.html(this.state.desc)
