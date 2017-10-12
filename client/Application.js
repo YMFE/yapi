f7b97f00b3d59d66dcecdc61050ef6da8f4d8e23
wenbo.dong <wenbo.dong@qunar.com>
2017-10-12 17:05:52 +0800

    fix: 删除页面内未使用的变量

--- client/Application.js
+++ client/Application.js
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 import { Home, Group, Project, Follows, AddProject, Login } from './containers/index';
 import { Alert } from 'antd';
-import { Link } from 'react-router-dom'
 import User from './containers/User/User.js';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
