4ef89f791d4d06811a67b5c34bbed59e650e475c
zwjamnsss <zwjamnsss@gmail.com>
2017-10-13 15:13:51 +0800

    opti: 安装插件

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@69ac3d9b2a64776501a0.js","css":"index@69ac3d9b2a64776501a0.css"},"lib":{"js":"lib@f88ed246a871afd46f3e.js"},"lib2":{"js":"lib2@34c977588e1eeac462a0.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@8a3568400b8e3dea6f1e.js","css":"index@8a3568400b8e3dea6f1e.css"},"lib":{"js":"lib@92e2c3937254a8f33f8a.js"},"lib2":{"js":"lib2@2e084b54b8a92fdaf766.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
