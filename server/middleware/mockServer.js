6373409daa19cea5b26c8d480fd9d60913dc833f
suxiaoxin <hellosean1025@163.com>
2017-11-02 17:50:34 +0800

    fix: advMock 期望空参数匹配 bug

--- server/middleware/mockServer.js
+++ server/middleware/mockServer.js
@@ -132,56 +132,58 @@ module.exports = async (ctx, next) => {
         }
 
         ctx.set("Access-Control-Allow-Origin", "*")
-        if (interfaceData.res_body_type === 'json') {
-            try {
-                let res = mockExtra(
+        let res;
+
+        res = interfaceData.res_body;
+        try {
+            if (interfaceData.res_body_type === 'json') {
+                res = mockExtra(
                     yapi.commons.json_parse(interfaceData.res_body),
                     {
                         query: ctx.request.query,
                         body: ctx.request.body
                     }
                 );
-
-                try{
+                try {
                     res = Mock.mock(res);
-                }catch(e){
+                } catch (e) {
                     yapi.commons.log(e, 'error')
-                    res = res;
-                }
-                
-                let context = {
-                    projectData: project,
-                    interfaceData: interfaceData,
-                    ctx: ctx,
-                    mockJson: res,
-                    resHeader: {},
-                    httpCode: 200,
-                    delay: 0
-                }
-                await yapi.emitHook('mock_after', context);
-                let handleMock = new Promise(resolve=>{
-                    setTimeout(()=>{
-                      resolve(true)
-                    }, context.delay)
-                  })
-                await handleMock;   
-                if(context.resHeader && typeof context.resHeader === 'object'){
-                    for(let i in context.resHeader){
-                        ctx.set(i, context.resHeader[i]);
-                    }
-                }
-                
-                ctx.status = context.httpCode;
-                return ctx.body = context.mockJson;
-            } catch (e) {
-                yapi.commons.log(e, 'error')
-                return ctx.body = {
-                    errcode: 400,
-                    errmsg: '解析出错，请检查。Error: ' + e.message,
-                    data: null
                 }
             }
+
+            let context = {
+                projectData: project,
+                interfaceData: interfaceData,
+                ctx: ctx,
+                mockJson: res,
+                resHeader: {},
+                httpCode: 200,
+                delay: 0
+            }
+            await yapi.emitHook('mock_after', context);
+            let handleMock = new Promise(resolve => {
+                setTimeout(() => {
+                    resolve(true)
+                }, context.delay)
+            })
+            await handleMock;
+            if (context.resHeader && typeof context.resHeader === 'object') {
+                for (let i in context.resHeader) {
+                    ctx.set(i, context.resHeader[i]);
+                }
+            }
+
+            ctx.status = context.httpCode;
+            return ctx.body = context.mockJson;
+        } catch (e) {
+            yapi.commons.log(e, 'error')
+            return ctx.body = {
+                errcode: 400,
+                errmsg: '解析出错，请检查。Error: ' + e.message,
+                data: null
+            }
         }
+
         return ctx.body = interfaceData.res_body;
     } catch (e) {
         console.error(e)
