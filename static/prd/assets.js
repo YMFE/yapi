a69f402a152f1357f51b06692070fae7ad02f5f5
suxiaoxin <hellosean1025@163.com>
2017-11-06 21:01:04 +0800

    chore: update static file

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@aeda4564fb6a9b08fa32.js","css":"index@aeda4564fb6a9b08fa32.css"},"lib":{"js":"lib@f6dd47c1f1d5bc6322ad.js"},"lib2":{"js":"lib2@dbcc326d4fe3e4473316.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@8c056000e73771860ca2.js","css":"index@8c056000e73771860ca2.css"},"lib":{"js":"lib@f6dd47c1f1d5bc6322ad.js"},"lib2":{"js":"lib2@dbcc326d4fe3e4473316.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
