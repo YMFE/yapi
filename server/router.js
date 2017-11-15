d7f560351900bb905a8f62564ba341e674c42ca7
wnstar <wnstar@iqing.in>
2017-11-15 12:37:23 +0800

    fix: 增加一个websocket路由钩子 钩子名add_ws_router 参数同 add_router

--- server/router.js
+++ server/router.js
@@ -9,41 +9,42 @@ const yapi = require('./yapi.js');
 const projectController = require('./controllers/project.js');
 const logController = require('./controllers/log.js');
 const followController = require('./controllers/follow.js');
+const { createAction } = require("./utils/commons.js")
 
 const router = koaRouter();
 
 const authLevel = {
-    admin: 0,
-    owner: 10,
-    dev: 20,
-    member:30,
-    guest:100
+	admin: 0,
+	owner: 10,
+	dev: 20,
+	member: 30,
+	guest: 100
 }
 
 let INTERFACE_CONFIG = {
-    interface: {
-        prefix: '/interface/',
-        controller: interfaceController
-    },
-    user: {
-        prefix: '/user/',
-        controller: userController
-    },
-    group: {
-        prefix: '/group/',
-        controller: groupController
-    },
-    project: {
-        prefix: '/project/',
-        controller: projectController
-    },
-    log: {
-        prefix: '/log/',
-        controller: logController
+	interface: {
+		prefix: '/interface/',
+		controller: interfaceController
+	},
+	user: {
+		prefix: '/user/',
+		controller: userController
+	},
+	group: {
+		prefix: '/group/',
+		controller: groupController
+	},
+	project: {
+		prefix: '/project/',
+		controller: projectController
+	},
+	log: {
+		prefix: '/log/',
+		controller: logController
 	},
 	follow: {
-        prefix: '/follow/',
-        controller: followController
+		prefix: '/follow/',
+		controller: followController
 	},
 	col: {
 		prefix: '/col/',
@@ -96,7 +97,7 @@ let routerConfig = {
 			"action": "getMemberList",
 			"path": "get_member_list",
 			"method": "get"
-		},{
+		}, {
 			action: 'get',
 			path: 'get',
 			method: 'get'
@@ -167,11 +168,11 @@ let routerConfig = {
 			"action": "project",
 			"path": "project",
 			"method": "get"
-		},{
+		}, {
 			"action": "avatar",
 			"path": "avatar",
 			"method": "get"
-		},{
+		}, {
 			action: "uploadAvatar",
 			path: "upload_avatar",
 			method: "post"
@@ -182,7 +183,7 @@ let routerConfig = {
 			"action": "upSet",
 			"path": "upset",
 			"method": "post"
-		},{
+		}, {
 			"action": "add",
 			"path": "add",
 			"method": "post"
@@ -246,7 +247,7 @@ let routerConfig = {
 		},
 		{
 			"action": "downloadCrx",
-			"path"  : "download_crx",
+			"path": "download_crx",
 			"method": "get"
 		},
 		{
@@ -283,19 +284,19 @@ let routerConfig = {
 			action: 'listByCat',
 			path: 'list_cat',
 			method: 'get'
-		},{
+		}, {
 			action: 'listByMenu',
 			path: 'list_menu',
 			method: 'get'
-		},{
+		}, {
 			action: 'addCat',
 			path: 'add_cat',
 			method: 'post'
-		},{
+		}, {
 			action: 'upCat',
 			path: 'up_cat',
 			method: 'post'
-		},{
+		}, {
 			action: 'delCat',
 			path: 'del_cat',
 			method: 'post'
@@ -312,11 +313,11 @@ let routerConfig = {
 		"action": "list",
 		"path": "list",
 		"method": "get"
-	},{
+	}, {
 		"action": "add",
 		"path": "add",
 		"method": "post"
-	},{
+	}, {
 		"action": "del",
 		"path": "del",
 		"method": "post"
@@ -325,48 +326,48 @@ let routerConfig = {
 		action: "addCol",
 		path: "add_col",
 		method: "post"
-	},{
+	}, {
 		action: 'addCaseList',
 		path: 'add_case_list',
-		method: 'post'	
-		
+		method: 'post'
+
 	}, {
 		action: "list",
 		path: "list",
 		method: "get"
-	},{
+	}, {
 		action: "getCaseList",
 		path: "case_list",
 		method: "get"
-	},{
+	}, {
 		action: "addCase",
 		path: "add_case",
 		method: "post"
-	},{
+	}, {
 		action: "upCase",
 		path: "up_case",
 		method: "post"
-	},{
+	}, {
 		action: "getCase",
 		path: "case",
 		method: "get"
-	},{
+	}, {
 		action: "upCol",
 		path: "up_col",
 		method: "post"
-	},{
+	}, {
 		action: "upCaseIndex",
 		path: "up_col_index",
 		method: "post"
-	},{
+	}, {
 		action: "delCol",
 		path: "del_col",
 		method: "get"
-	},{
+	}, {
 		action: "delCase",
 		path: "del_case",
 		method: "get"
-	},{
+	}, {
 		action: "runCaseScript",
 		path: "run_script",
 		method: "post"
@@ -413,56 +414,30 @@ let routerConfig = {
 
 let pluginsRouterPath = [];
 
-function addPluginRouter(config){
-	if(!config.path || !config.controller || !config.action){
+function addPluginRouter(config) {
+	if (!config.path || !config.controller || !config.action) {
 		throw new Error('Plugin Route config Error');
 	}
 	let method = config.method || 'GET';
 	let routerPath = '/plugin/' + config.path;
-	if(pluginsRouterPath.indexOf(routerPath) > -1){
+	if (pluginsRouterPath.indexOf(routerPath) > -1) {
 		throw new Error('Plugin Route path conflict, please try rename the path')
 	}
 	pluginsRouterPath.push(routerPath);
-	createAction(config.controller, config.action, routerPath, method);
+	createAction(router, "/api", config.controller, config.action, routerPath, method,false);
 }
 
 yapi.emitHookSync('add_router', addPluginRouter);
 
-for(let ctrl in routerConfig){
-    let actions = routerConfig[ctrl];
-    actions.forEach( (item) => {
-				let routerController = INTERFACE_CONFIG[ctrl].controller;
-				let routerPath = INTERFACE_CONFIG[ctrl].prefix + item.path;
-        createAction(routerController, item.action, routerPath, item.method);
-    })
+for (let ctrl in routerConfig) {
+	let actions = routerConfig[ctrl];
+	actions.forEach((item) => {
+		let routerController = INTERFACE_CONFIG[ctrl].controller;
+		let routerPath = INTERFACE_CONFIG[ctrl].prefix + item.path;
+		createAction(router, "/api", routerController, item.action, routerPath, item.method);
+	})
 }
 
-
-/**
- *
- * @param {*} routerController controller
- * @param {*} path  routerPath
- * @param {*} method request_method , post get put delete ...
- * @param {*} action controller action_name
- */
-function createAction(routerController, action, path, method) {
-    router[method]("/api" +  path, async (ctx) => {
-        let inst = new routerController(ctx);
-				try{
-					await inst.init(ctx);
-					
-									if (inst.$auth === true) {
-											await inst[action].call(inst, ctx);
-									} else {
-											ctx.body = yapi.commons.resReturn(null, 40011, '请登录...');
-									}
-				}catch(err){
-					ctx.body = yapi.commons.resReturn(null, 40011, '服务器出错...');
-					yapi.commons.log(err, 'error')
-				}
-        
-    });
-}
 
 
 module.exports = router;
