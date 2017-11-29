dbd56db77b5fb25d3af84f661577870898cc5df6
suxiaoxin <hellosean1025@163.com>
2017-11-29 21:52:39 +0800

    opti: 重构接口请求和测试部分代码

--- client/common.js
+++ client/common.js
@@ -25,7 +25,7 @@ const roleAction = {
 function isJson(json){
   if(!json) return false;
   try{
-    json = json5.parse(json);
+    json = JSON.parse(json);
     return json;
   }catch(e){
     return false;
@@ -33,6 +33,24 @@ function isJson(json){
 }
 
 exports.isJson = isJson;
+
+function isJson5(json){
+  if(!json) return false;
+  try{
+    json = json5.parse(json);
+    return json;
+  }catch(e){
+    return false;
+  }
+}
+
+function deepCopyJson(json){
+  return JSON.parse(JSON.stringify(json));
+}
+
+exports.deepCopyJson = deepCopyJson;
+
+exports.isJson5 = isJson5;
 
 exports.checkAuth = (action, role)=>{
   return Roles[roleAction[action]] <= Roles[role];
@@ -305,4 +323,8 @@ exports.joinPath = (domain, joinPath) =>{
     joinPath = joinPath.substr(1);
   }
   return domain + joinPath;
+}
+
+exports.safeArray = (arr) => {
+  return Array.isArray(arr) ? arr :  [];
 }
---
+++ client/components/AceEditor/AceEditor.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import mockEditor from './mockEditor'
+import PropTypes from 'prop-types'
+import './AceEditor.scss'
+
+const ModeMap = {
+  'javascript' : 'ace/mode/javascript',
+  'json' : 'ace/mode/json',
+  'text' : 'ace/mode/text',
+  'xml'  : 'ace/mode/xml',
+  'html' : 'ace/mode/html'
+}
+
+function getMode(mode){
+  return ModeMap[mode] || ModeMap.text
+}
+
+class AceEditor extends React.PureComponent {
+  constructor(props){
+    super(props);
+  }
+
+  static propTypes = {
+    data: PropTypes.any,
+    onChange: PropTypes.func,
+    className: PropTypes.string,
+    mode: PropTypes.string, //enum[json, text, javascript], default is javascript
+    readOnly: PropTypes.bool,
+    callback: PropTypes.func,
+    style: PropTypes.object,
+    fullScreen: PropTypes.bool
+  }
+
+  componentDidMount(){
+    this.editor = mockEditor({
+      container: this.editorElement,
+      data: this.props.data,
+      onChange: this.props.onChange,
+      readOnly: this.props.readOnly,
+      fullScreen: this.props.fullScreen
+    })
+
+    let mode = this.props.mode || 'javascript';
+    this.editor.editor.getSession().setMode(getMode(mode));
+    if(typeof this.props.callback === 'function'){
+      this.props.callback(this.editor.editor)
+    }
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(!this.editor) return;
+    if(nextProps.data !== this.props.data && this.editor.getValue() !== nextProps.data){
+      this.editor.setValue(nextProps.data);
+      let mode = nextProps.mode || 'javascript';      
+      this.editor.editor.getSession().setMode(getMode(mode));
+      this.editor.editor.clearSelection();
+    }    
+  }
+
+  render() {
+    return <div className={this.props.className} style={this.props.style || {width: '100%', height: '200px'}} ref={editor=>{
+      this.editorElement=editor  
+    }} ></div>
+  }
+}
+
+export default AceEditor;
