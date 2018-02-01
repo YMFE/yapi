5fdaabfdbfcff1e7708a30cc35db6cf77fc9076c
suxiaoxin <hellosean1025@163.com>
2018-02-01 10:57:14 +0800

    chore: update static file

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@2c383ec099ba442786d4.js","css":"index@2c383ec099ba442786d4.css"},"lib":{"js":"lib@9f78176f032cca64fbc0.js"},"lib2":{"js":"lib2@2bba6eec8fa20f589580.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
-=======
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@8128d654817e115dcb7a.js","css":"index@8128d654817e115dcb7a.css"},"lib":{"js":"lib@a660806647f646177f7c.js"},"lib2":{"js":"lib2@96e1b2b1489d5f43b9e4.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
->>>>>>> dev
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@2b452c4c3003a9e0d9df.js","css":"index@2b452c4c3003a9e0d9df.css"},"lib":{"js":"lib@ce3f780ffeaf5fbe2c75.js"},"lib2":{"js":"lib2@4beef1db805afefa676c.js"},"manifest":{"js":"manifest@b67af9f8b578904e66c5.js"}}
