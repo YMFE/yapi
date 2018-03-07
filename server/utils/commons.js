5d6478048b500048ccf6d84fbd3189b350111ceb
suxiaoxin <hellosean1025@163.com>
2018-03-07 17:39:48 +0800

    fix: jsf

--- server/utils/commons.js
+++ server/utils/commons.js
@@ -1,31 +1,49 @@
-const fs = require('fs-extra');
-const path = require('path');
-const yapi = require('../yapi.js');
-const sha1 = require('sha1');
-const logModel = require('../models/log.js');
-const projectModel = require('../models/project.js');
-const interfaceColModel = require('../models/interfaceCol.js');
-const interfaceCaseModel = require('../models/interfaceCase.js');
-const interfaceModel = require('../models/interface.js');
-const json5 = require('json5');
-const _ = require('underscore');
-const Ajv = require('ajv');
+const fs = require("fs-extra");
+const path = require("path");
+const yapi = require("../yapi.js");
+const sha1 = require("sha1");
+const logModel = require("../models/log.js");
+const projectModel = require("../models/project.js");
+const interfaceColModel = require("../models/interfaceCol.js");
+const interfaceCaseModel = require("../models/interfaceCase.js");
+const interfaceModel = require("../models/interface.js");
+const json5 = require("json5");
+const _ = require("underscore");
+const Ajv = require("ajv");
 const ajv = new Ajv({
   allErrors: true,
   coerceTypes: true,
   useDefaults: true,
   removeAdditional: true
 });
-var localize = require('ajv-i18n');
+var localize = require("ajv-i18n");
+const ejs = require("easy-json-schema");
 
-const ejs = require('easy-json-schema');
+const jsf = require('json-schema-faker');  
+const defaultOptions = {
+  failOnInvalidTypes: false,
+  failOnInvalidFormat: false
+}
+
+exports.schemaToJson = function(schema, options={}){  
+  Object.assign(options, defaultOptions)
+  jsf.option(options);
+  let result
+  try{
+    result = jsf(schema)
+  }catch(err){
+    result = err.message
+  }
+  jsf.option(defaultOptions)
+  return result;
+}
 
 exports.resReturn = (data, num, errmsg) => {
   num = num || 0;
 
   return {
     errcode: num,
-    errmsg: errmsg || '成功！',
+    errmsg: errmsg || "成功！",
     data: data
   };
 };
@@ -35,18 +53,18 @@ exports.log = (msg, type) => {
     return;
   }
 
-  type = type || 'log';
+  type = type || "log";
 
   let f;
 
   switch (type) {
-    case 'log':
+    case "log":
       f = console.log; // eslint-disable-line
       break;
-    case 'warn':
+    case "warn":
       f = console.warn; // eslint-disable-line
       break;
-    case 'error':
+    case "error":
       f = console.error; // eslint-disable-line
       break;
     default:
@@ -54,28 +72,28 @@ exports.log = (msg, type) => {
       break;
   }
 
-  f(type + ':', msg);
+  f(type + ":", msg);
 
   let date = new Date();
   let year = date.getFullYear();
   let month = date.getMonth() + 1;
 
-  let logfile = path.join(yapi.WEBROOT_LOG, year + '-' + month + '.log');
+  let logfile = path.join(yapi.WEBROOT_LOG, year + "-" + month + ".log");
 
-  if (typeof msg === 'object') {
+  if (typeof msg === "object") {
     if (msg instanceof Error) msg = msg.message;
     else msg = JSON.stringify(msg);
   }
 
   // let data = (new Date).toLocaleString() + '\t|\t' + type + '\t|\t' + msg + '\n';
-  let data = `[ ${(new Date).toLocaleString()} ] [ ${type} ] ${msg}\n`;
+  let data = `[ ${new Date().toLocaleString()} ] [ ${type} ] ${msg}\n`;
 
   fs.writeFileSync(logfile, data, {
-    flag: 'a'
+    flag: "a"
   });
 };
 
-exports.fileExist = (filePath) => {
+exports.fileExist = filePath => {
   try {
     return fs.statSync(filePath).isFile();
   } catch (err) {
@@ -94,8 +112,8 @@ exports.fieldSelect = (data, field) => {
 
   var arr = {};
 
-  field.forEach((f) => {
-    (typeof data[f] !== 'undefined') && (arr[f] = data[f]);
+  field.forEach(f => {
+    typeof data[f] !== "undefined" && (arr[f] = data[f]);
   });
 
   return arr;
@@ -105,7 +123,7 @@ exports.rand = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-exports.json_parse = (json) => {
+exports.json_parse = json => {
   try {
     return json5.parse(json);
   } catch (e) {
@@ -114,24 +132,27 @@ exports.json_parse = (json) => {
 };
 
 exports.randStr = () => {
-  return Math.random().toString(36).substr(2);
+  return Math.random()
+    .toString(36)
+    .substr(2);
 };
-exports.getIp = (ctx) => {
+exports.getIp = ctx => {
   let ip;
   try {
-    ip = ctx.ip.match(/\d+.\d+.\d+.\d+/) ? ctx.ip.match(/\d+.\d+.\d+.\d+/)[0] : 'localhost';
+    ip = ctx.ip.match(/\d+.\d+.\d+.\d+/)
+      ? ctx.ip.match(/\d+.\d+.\d+.\d+/)[0]
+      : "localhost";
   } catch (e) {
     ip = null;
   }
-  return ip
-}
-
+  return ip;
+};
 
 exports.generatePassword = (password, passsalt) => {
   return sha1(password + sha1(passsalt));
 };
 
-exports.expireDate = (day) => {
+exports.expireDate = day => {
   let date = new Date();
   date.setTime(date.getTime() + day * 86400000);
   return date;
@@ -139,26 +160,35 @@ exports.expireDate = (day) => {
 
 exports.sendMail = (options, cb) => {
   if (!yapi.mail) return false;
-  options.subject = options.subject ? options.subject + '-YApi 平台' : 'YApi 平台';
+  options.subject = options.subject
+    ? options.subject + "-YApi 平台"
+    : "YApi 平台";
 
-  cb = cb || function (err) {
-    if (err) {
-      yapi.commons.log('send mail ' + options.to + ' error,' + err.message, 'error');
+  cb =
+    cb ||
+    function(err) {
+      if (err) {
+        yapi.commons.log(
+          "send mail " + options.to + " error," + err.message,
+          "error"
+        );
+      } else {
+        yapi.commons.log("send mail " + options.to + " success");
+      }
+    };
 
-    } else {
-      yapi.commons.log('send mail ' + options.to + ' success');
-    }
-  };
-  
   try {
-    yapi.mail.sendMail({
-      from: yapi.WEBCONFIG.mail.from,
-      to: options.to,
-      subject: options.subject,
-      html: options.contents
-    }, cb);
+    yapi.mail.sendMail(
+      {
+        from: yapi.WEBCONFIG.mail.from,
+        to: options.to,
+        subject: options.subject,
+        html: options.contents
+      },
+      cb
+    );
   } catch (e) {
-    yapi.commons.log(e.message, 'error')
+    yapi.commons.log(e.message, "error");
     console.error(e.message); // eslint-disable-line
   }
 };
@@ -176,9 +206,9 @@ exports.filterRes = (list, rules) => {
     let filteredRes = {};
 
     rules.forEach(rule => {
-      if (typeof rule == 'string') {
+      if (typeof rule == "string") {
         filteredRes[rule] = item[rule];
-      } else if (typeof rule == 'object') {
+      } else if (typeof rule == "object") {
         filteredRes[rule.alias] = item[rule.key];
       }
     });
@@ -188,36 +218,37 @@ exports.filterRes = (list, rules) => {
 };
 
 exports.handleVarPath = (pathname, params) => {
-
   function insertParams(name) {
     if (!_.find(params, { name: name })) {
       params.push({
         name: name,
-        desc: ''
-      })
+        desc: ""
+      });
     }
   }
 
   if (!pathname) return;
-  if (pathname.indexOf(':') !== -1) {
-    let paths = pathname.split("/"), name, i;
+  if (pathname.indexOf(":") !== -1) {
+    let paths = pathname.split("/"),
+      name,
+      i;
     for (i = 1; i < paths.length; i++) {
-      if (paths[i] && paths[i][0] === ':') {
+      if (paths[i] && paths[i][0] === ":") {
         name = paths[i].substr(1);
-        insertParams(name)
+        insertParams(name);
       }
     }
   }
-  pathname.replace(/\{(.+?)\}/g, function (str, match) {
-    insertParams(match)
-  })
-}
+  pathname.replace(/\{(.+?)\}/g, function(str, match) {
+    insertParams(match);
+  });
+};
 
 /**
  * 验证一个 path 是否合法
  * path第一位必需为 /, path 只允许由 字母数字-/_:.{}= 组成
  */
-exports.verifyPath = (path) => {
+exports.verifyPath = path => {
   if (/^\/[a-zA-Z0-9\-\/_:\.\{\}\=]*$/.test(path)) {
     return true;
   } else {
@@ -226,16 +257,16 @@ exports.verifyPath = (path) => {
 };
 
 /**
- * 沙盒执行 js 代码 
+ * 沙盒执行 js 代码
  * @sandbox Object context
  * @script String script
  * @return sandbox
- * 
+ *
  * @example let a = sandbox({a: 1}, 'a=2')
  * a = {a: 2}
  */
 exports.sandbox = (sandbox, script) => {
-  const vm = require('vm');
+  const vm = require("vm");
   sandbox = sandbox || {};
   script = new vm.Script(script);
   const context = new vm.createContext(sandbox);
@@ -243,16 +274,16 @@ exports.sandbox = (sandbox, script) => {
     timeout: 3000
   });
   return sandbox;
-}
+};
 
 function trim(str) {
   if (!str) {
     return str;
   }
 
-  str = str + '';
+  str = str + "";
 
-  return str.replace(/(^\s*)|(\s*$)/g, '');
+  return str.replace(/(^\s*)|(\s*$)/g, "");
 }
 
 function ltrim(str) {
@@ -260,9 +291,9 @@ function ltrim(str) {
     return str;
   }
 
-  str = str + '';
+  str = str + "";
 
-  return str.replace(/(^\s*)/g, '');
+  return str.replace(/(^\s*)/g, "");
 }
 
 function rtrim(str) {
@@ -270,9 +301,9 @@ function rtrim(str) {
     return str;
   }
 
-  str = str + '';
+  str = str + "";
 
-  return str.replace(/(\s*$)/g, '');
+  return str.replace(/(\s*$)/g, "");
 }
 
 exports.trim = trim;
@@ -286,7 +317,12 @@ exports.rtrim = rtrim;
  * @return Object {a: 'ab', b: 123}
  */
 exports.handleParams = (params, keys) => {
-  if (!params || typeof params !== 'object' || !keys || typeof keys !== 'object') {
+  if (
+    !params ||
+    typeof params !== "object" ||
+    !keys ||
+    typeof keys !== "object"
+  ) {
     return false;
   }
 
@@ -294,11 +330,14 @@ exports.handleParams = (params, keys) => {
     var filter = keys[key];
     if (params[key]) {
       switch (filter) {
-        case 'string': params[key] = trim(params[key] + '');
+        case "string":
+          params[key] = trim(params[key] + "");
           break;
-        case 'number': params[key] = !isNaN(params[key]) ? parseInt(params[key], 10) : 0;
+        case "number":
+          params[key] = !isNaN(params[key]) ? parseInt(params[key], 10) : 0;
           break;
-        default: params[key] = trim(params + '');
+        default:
+          params[key] = trim(params + "");
       }
     }
   }
@@ -311,20 +350,19 @@ exports.validateParams = (schema, params) => {
   schema.additionalProperties = false;
   const validate = ajv.compile(schema);
   let valid = validate(params);
-  let message = '请求参数 ';
+  let message = "请求参数 ";
   if (!valid) {
     localize.zh(validate.errors);
-    message += ajv.errorsText(validate.errors, { separator: '\n' });
+    message += ajv.errorsText(validate.errors, { separator: "\n" });
   }
 
   return {
     valid: valid,
     message: message
-  }
-}
+  };
+};
 
-
-exports.saveLog = (logData) => {
+exports.saveLog = logData => {
   try {
     let logInst = yapi.getInst(logModel);
     let data = {
@@ -337,10 +375,9 @@ exports.saveLog = (logData) => {
     };
     logInst.save(data).then();
   } catch (e) {
-    yapi.commons.log(e, 'error'); // eslint-disable-line
+    yapi.commons.log(e, "error"); // eslint-disable-line
   }
 };
-
 
 /**
  *
@@ -352,130 +389,156 @@ exports.saveLog = (logData) => {
  * @param {*} action controller action_name
  * @param {*} ws enable ws
  */
-exports.createAction = (router, baseurl, routerController, action, path, method, ws) => {
-  router[method](baseurl + path, async (ctx) => {
+exports.createAction = (
+  router,
+  baseurl,
+  routerController,
+  action,
+  path,
+  method,
+  ws
+) => {
+  router[method](baseurl + path, async ctx => {
     let inst = new routerController(ctx);
     try {
       await inst.init(ctx);
-      if (inst.schemaMap && typeof inst.schemaMap === 'object' && inst.schemaMap[action]) {
-        ctx.params = Object.assign({}, ctx.request.query, ctx.request.body, ctx.params);
-        let validResult = yapi.commons.validateParams(inst.schemaMap[action], ctx.params);
+      if (
+        inst.schemaMap &&
+        typeof inst.schemaMap === "object" &&
+        inst.schemaMap[action]
+      ) {
+        ctx.params = Object.assign(
+          {},
+          ctx.request.query,
+          ctx.request.body,
+          ctx.params
+        );
+        let validResult = yapi.commons.validateParams(
+          inst.schemaMap[action],
+          ctx.params
+        );
 
         if (!validResult.valid) {
-          return ctx.body = yapi.commons.resReturn(null, 400, validResult.message);
+          return (ctx.body = yapi.commons.resReturn(
+            null,
+            400,
+            validResult.message
+          ));
         }
       }
       if (inst.$auth === true) {
         await inst[action].call(inst, ctx);
       } else {
         if (ws === true) {
-          ctx.ws.send('请登录...');
+          ctx.ws.send("请登录...");
         } else {
-          ctx.body = yapi.commons.resReturn(null, 40011, '请登录...');
+          ctx.body = yapi.commons.resReturn(null, 40011, "请登录...");
         }
       }
     } catch (err) {
-      ctx.body = yapi.commons.resReturn(null, 40011, '服务器出错...');
-      yapi.commons.log(err, 'error')
+      ctx.body = yapi.commons.resReturn(null, 40011, "服务器出错...");
+      yapi.commons.log(err, "error");
     }
-
   });
-}
+};
 
 /**
- * 
+ *
  * @param {*} params 接口定义的参数
  * @param {*} val  接口case 定义的参数值
  */
-function handleParamsValue (params, val){
-    let value = {};
-    try {
-      params = params.toObject();
-    } catch (e) { }
-    if (params.length === 0 || val.length === 0) {
-      return params;
-    }
-    val.forEach((item) => {
-      value[item.name] = item;
-    })
-    params.forEach((item, index) => {
-      if (!value[item.name] || typeof value[item.name] !== 'object') return null;
-      params[index].value = value[item.name].value;
-      if (!_.isUndefined(value[item.name].enable)) {
-        params[index].enable = value[item.name].enable
-      }
-    })
+function handleParamsValue(params, val) {
+  let value = {};
+  try {
+    params = params.toObject();
+  } catch (e) {}
+  if (params.length === 0 || val.length === 0) {
     return params;
   }
-
-  exports.handleParamsValue = handleParamsValue;
-
-  exports.getCaseList = async function getCaseList(id) {
-
-    const caseInst = yapi.getInst(interfaceCaseModel)
-    const colInst = yapi.getInst(interfaceColModel)
-    const projectInst = yapi.getInst(projectModel)
-    const interfaceInst = yapi.getInst(interfaceModel)
-
-    let resultList = await caseInst.list(id, 'all');
-    let colData = await colInst.get(id);
-    for (let index = 0; index < resultList.length; index++) {
-      let result = resultList[index].toObject();
-      let data = await interfaceInst.get(result.interface_id);
-      if (!data) {
-        await caseInst.del(result._id);
-        continue;
-      }
-      let projectData = await projectInst.getBaseInfo(data.project_id);
-      result.path = projectData.basepath + data.path;
-      result.method = data.method;
-      result.title = data.title;
-      result.req_body_type = data.req_body_type;
-      result.req_headers = handleParamsValue(data.req_headers, result.req_headers);
-      result.res_body_type = data.res_body_type;
-      result.req_body_form = handleParamsValue(data.req_body_form, result.req_body_form)
-      result.req_query = handleParamsValue(data.req_query, result.req_query)
-      result.req_params = handleParamsValue(data.req_params, result.req_params)
-      resultList[index] = result;
+  val.forEach(item => {
+    value[item.name] = item;
+  });
+  params.forEach((item, index) => {
+    if (!value[item.name] || typeof value[item.name] !== "object") return null;
+    params[index].value = value[item.name].value;
+    if (!_.isUndefined(value[item.name].enable)) {
+      params[index].enable = value[item.name].enable;
     }
-    resultList = resultList.sort((a, b) => {
-      return a.index - b.index;
-    });
-    let ctxBody = yapi.commons.resReturn(resultList);
-    ctxBody.colData = colData;
-    return ctxBody;
+  });
+  return params;
 }
 
+exports.handleParamsValue = handleParamsValue;
 
+exports.getCaseList = async function getCaseList(id) {
+  const caseInst = yapi.getInst(interfaceCaseModel);
+  const colInst = yapi.getInst(interfaceColModel);
+  const projectInst = yapi.getInst(projectModel);
+  const interfaceInst = yapi.getInst(interfaceModel);
+
+  let resultList = await caseInst.list(id, "all");
+  let colData = await colInst.get(id);
+  for (let index = 0; index < resultList.length; index++) {
+    let result = resultList[index].toObject();
+    let data = await interfaceInst.get(result.interface_id);
+    if (!data) {
+      await caseInst.del(result._id);
+      continue;
+    }
+    let projectData = await projectInst.getBaseInfo(data.project_id);
+    result.path = projectData.basepath + data.path;
+    result.method = data.method;
+    result.title = data.title;
+    result.req_body_type = data.req_body_type;
+    result.req_headers = handleParamsValue(
+      data.req_headers,
+      result.req_headers
+    );
+    result.res_body_type = data.res_body_type;
+    result.req_body_form = handleParamsValue(
+      data.req_body_form,
+      result.req_body_form
+    );
+    result.req_query = handleParamsValue(data.req_query, result.req_query);
+    result.req_params = handleParamsValue(data.req_params, result.req_params);
+    resultList[index] = result;
+  }
+  resultList = resultList.sort((a, b) => {
+    return a.index - b.index;
+  });
+  let ctxBody = yapi.commons.resReturn(resultList);
+  ctxBody.colData = colData;
+  return ctxBody;
+};
 
 function convertString(variable) {
   if (variable instanceof Error) {
-    return variable.name + ': ' + variable.message;
+    return variable.name + ": " + variable.message;
   }
   try {
-    return JSON.stringify(variable, null, '   ');
+    return JSON.stringify(variable, null, "   ");
   } catch (err) {
-    return variable || '';
+    return variable || "";
   }
 }
 
-exports.runCaseScript = async function runCaseScript(params){
+exports.runCaseScript = async function runCaseScript(params) {
   let script = params.script;
   if (!script) {
-    return yapi.commons.resReturn('ok');
+    return yapi.commons.resReturn("ok");
   }
   const logs = [];
   const context = {
-    assert: require('assert'),
+    assert: require("assert"),
     status: params.response.status,
     body: params.response.body,
     header: params.response.header,
     records: params.records,
     params: params.params,
-    log: (msg) => {
-      logs.push('log: ' + convertString(msg))
+    log: msg => {
+      logs.push("log: " + convertString(msg));
     }
-  }
+  };
 
   let result = {};
   try {
@@ -485,6 +548,6 @@ exports.runCaseScript = async function runCaseScript(params){
   } catch (err) {
     logs.push(convertString(err));
     result.logs = logs;
-    return yapi.commons.resReturn(result, 400, err.name + ": " + err.message)
+    return yapi.commons.resReturn(result, 400, err.name + ": " + err.message);
   }
-}
+};
