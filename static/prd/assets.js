a52b9f3f55d3455293dedb1a95615cd23c712382
gaoxiaolin.gao <gaoxiaolin.gao@qunar.com>
2018-05-23 17:08:34 +0800

    chore: update static file

--- static/prd/assets.js
+++ static/prd/assets.js
@@ -1,1 +1,1 @@
-window.WEBPACK_ASSETS = {"index.js":{"js":"index@4b5f0eef4524a3932c0f.js","css":"index@4b5f0eef4524a3932c0f.css"},"lib":{"js":"lib.min.js"},"lib2":{"js":"lib2.min.js"},"lib3":{"js":"lib3.min.js"},"manifest":{"js":"manifest.min.js"}}
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@54b1a9f9844371285cc7.js","css":"index@54b1a9f9844371285cc7.css"},"lib":{"js":"lib.min.js"},"lib2":{"js":"lib2.min.js"},"lib3":{"js":"lib3.min.js"},"manifest":{"js":"manifest.min.js"}}
