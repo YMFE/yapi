a64a4ea8aa218594827b471e011a2d64437f6f32
edenhliu <edenhliu@tencent.com>
2018-09-02 16:23:59 +0800

    feat: eslint update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: ['eslint-config-zman']
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'import'],
+  rules: {
+    indent: ['off', 2],
+    'react/display-name': ['off'],
+    'react/jsx-indent': ['error', 2],
+    'comma-dangle': ['error', 'never'],
+    'no-console': ['off'],
+    'import/no-unresolved': ['off'],
+    'react/no-find-dom-node': ['off'],
+    // "react/no-unescaped-entities": 0
+  },
 };
