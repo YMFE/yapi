e7db591a4441cf56058b4a31820d8aaaf34f9c39
edenhliu <edenhliu@tencent.com>
2018-09-03 21:24:50 +0800

    feat: build client

---
+++ static/prd/assets.js
@@ -0,0 +1,1 @@
+window.WEBPACK_ASSETS = {"index.js":{"js":"index@5e1f41d9f490eabc1acf.js","css":"index@5e1f41d9f490eabc1acf.css"},"lib":{"js":"lib@2eaadd00ee413cc8b2e6.js"},"lib2":{"js":"lib2@374656f68f44847f09f7.js"},"lib3":{"js":"lib3@a366906db3c48828a3f9.js"},"manifest":{"js":"manifest@f2f4bd774d6c221b3d5f.js"}}
