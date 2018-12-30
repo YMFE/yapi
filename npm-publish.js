e4c72243974bd19f8ff9e2f759bf9f654f0bc4c9
suwenxiong <suwenxiong@meituan.com>
2018-12-30 12:34:56 +0800

    feat: version

--- npm-publish.js
+++ npm-publish.js
@@ -12,8 +12,8 @@ console.log ('exec: git pull origin master');
 shell.exec ('git pull origin master');
 let a = shell.exec (`git tag |grep ${version} |wc -l`);
 if (a && parseInt (a) > 0) {
-  shell.exec ('git tag -d ' + version);
-  shell.exec ('git push origin :' + version);
+  console.error('存在的 git tag')
+  process.exit(1)
 }
 shell.exec ('git tag ' + version);
 shell.exec ('git push origin ' + version);
