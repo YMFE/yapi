6dab1df55ebcc2190517ad7b263dfdd42c29e669
suwenxiong <suwenxiong@meituan.com>
2019-02-04 08:09:52 +0800

    fix: 修复版本提醒功能bug和默认在新版本开启了版本提醒功能

--- client/components/Notify/Notify.js
+++ client/components/Notify/Notify.js
@@ -7,14 +7,14 @@ export default class Notify extends Component {
     super(props);
     this.state = {
       newVersion: process.env.version,
-      version: process.env.version
+      version: 'v1.0'
     };
   }
 
   componentDidMount() {
-    axios.get('http://yapi.demo.qunar.com/publicapi/versions').then(req => {
+    axios.get('https://www.easy-mock.com/mock/5c2851e3d84c733cb500c3b9/yapi/versions').then(req => {
       if (req.status === 200) {
-        this.setState({ newVersion: req.data[0].version });
+        this.setState({ newVersion: req.data.data[0] });
       } else {
         message.error('无法获取新版本信息！');
       }
