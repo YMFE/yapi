c02cc32feb6f75eaf0df573aecd65ea9519b1617
yanxianhe <xianhe_yan@sina.com>
2022-05-13 16:20:03 +0800

    update README.md yapi_start.sh

--- docker-compose/dockerfile/yapi_start.sh
+++ docker-compose/dockerfile/yapi_start.sh
@@ -1,32 +1,20 @@
 #!/usr/bin/env bash
 
 set -x
-vendors=/data/yapi/my-yapi/vendors
-
-checkmgfun(){
-    mgPID=`netstat -tlpn | grep 27017`
-    if [ "$mgPID" != "" ];
-    then
-        echo "假设根据vendors是否存执行具体脚本"
-        yapifun
-    else
-        nohup mongod --bind_ip_all > /data/runmongod.log 2>&1 &
-        sleep 3
-        yapifun
-    fi
-}
+myapi=/opt/yapi/my-yapi
+vendors=$myapi/vendors
 
 yapifun(){
     mflg=`test -d $vendors && echo "true" || echo "false"`
     if $mflg
     then
         echo "vendors 目录存在执行　node "
-        cd $vendors
-        node ./server/app.js 
+        cd $myapi
+        node vendors/server/app.js 
     else
         echo "vendors 目录不存在执行yapi server "
         yapi server
     fi
 }
 
-checkmgfun
+yapifun
